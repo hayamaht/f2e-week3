e6f94aab969fee324c2db610eddbf1c87f48812c
hayama <hayamaroom@gmail.com>
2018-06-21 02:35:30 +0800

    Apply Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatTableModule,
+  MatMenuModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatCardModule,
+  MatInputModule,
+  MatPaginatorModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -19,11 +32,23 @@ import { InMemoryDataService } from './in-memory-data.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    ),
+    MatButtonModule,
+    MatTableModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-products',
@@ -10,7 +12,11 @@ import { Product } from '../product';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[];
+  products = new MatTableDataSource<Product>();
+  selection = new SelectionModel<Product>(true, []);
+  displayedColumns = [ 'select', 'name', 'price', 'discount', 'inventories', 'status' ];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(
     private productService: ProductService
@@ -20,10 +26,25 @@ export class ProductsComponent implements OnInit {
     this.getProducts();
   }
 
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.products.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.products.data.forEach(row => this.selection.select(row));
+  }
+
   getProducts() {
     this.productService
       .getAll()
-      .subscribe(products => this.products = products);
+      .subscribe(products => {
+        this.products.data = products;
+        this.products.paginator = this.paginator;
+      });
   }
 
 }
